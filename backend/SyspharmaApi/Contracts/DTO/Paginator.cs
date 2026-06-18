56fdf4f06fdd8e4c5ccffd97069883b9c1e57315
Paulo Ricardo <pauloricardosmite@outlook.com>
2026-06-18 20:17:15 -0300

    Criado o contrato genérico para retornos com paginação

---
+++ backend/SyspharmaApi/Contracts/DTO/Paginator.cs
@@ -0,0 +1,9 @@
+﻿namespace SyspharmaApi.Contracts.DTO
+{
+    public class Paginator<T>(IEnumerable<T> responseList, int page = 0, int pagesize = 10) where T: class
+    {
+        public int Page { get; set; } = page;
+        public int Pagesize { get; set; } = pagesize;
+        public IEnumerable<T> ResponseList { get; set; } = responseList;
+    }
+}
