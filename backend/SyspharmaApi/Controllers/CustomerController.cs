d25916717fef7f22006685795e5e1d18737057bb
Paulo Ricardo <pauloricardosmite@outlook.com>
2026-06-19 13:57:33 -0300

    Removido o Paginator --> desnecessário. Vou usar o próprio método GET padrão sem encapsulamento extra

--- backend/SyspharmaApi/Controllers/CustomerController.cs
+++ backend/SyspharmaApi/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SyspharmaApi.Context;
+using SyspharmaApi.Contracts.DTO;
 using SyspharmaApi.Helpers;
 using SyspharmaApi.Models;
 using System.Net;
